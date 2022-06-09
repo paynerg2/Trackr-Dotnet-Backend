66280cfcb67e3dcb57de38261d375693deb66d84
Rodney Payne <paynerg2@gmail.com>
2022-06-08 20:22:40 -0400

    Add basic auth controller

---
+++ Trackr/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Trackr.Data;
+using Trackr.Models;
+
+namespace Trackr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<User> userManager,
+            ILogger<AccountController> logger, IMapper mapper)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration attempt for {userDTO.Email}");
+            
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var user = _mapper.Map<User>(userDTO);
+                user.UserName = userDTO.Email;
+                // Automatically hashes and stores password, etc.
+                var result = await _userManager.CreateAsync(user);
+
+                if(!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    // TODO: Probably check for and send useful information to the user, e.g. "Failed because email is already taken"
+                    return BadRequest(ModelState);
+                }
+
+                return Accepted();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in {nameof(Register)}.");
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] LoginUserDTO userDTO)
+        {
+            _logger.LogInformation($"Login attempt from {userDTO.Email}.");
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                //var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password,
+                //    isPersistent: false, lockoutOnFailure: false);
+
+                //if (!result.Succeeded)
+                //{
+                //    return Unauthorized(userDTO);
+                //}
+
+                return Accepted();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in {nameof(Login)}.");
+                return StatusCode(500);
+            }
+        }
+    }
+}
