2f48cc952428cbbb63453e23b8cc2f71e2511a14
Rodney Payne <paynerg2@gmail.com>
2022-06-08 20:17:53 -0400

    Extract enums into static classes
    - Avoid 'magic strings'
    - Share constant strings in multiple locations
    - Fields property included to use as RegEx for matching

---
+++ Trackr/Utils/Types/InterviewResponseType.cs
@@ -0,0 +1,12 @@
+﻿namespace Trackr.Utils.Types
+{
+    public static class InterviewResponseType
+    {
+        public const string None = "none";
+        public const string Passed = "passed";
+        public const string Rejected = "rejected";
+        public const string Offer = "offer";
+
+        public const string Fields = $"{None}|{Passed}|{Rejected}|{Offer}";
+    }
+}
